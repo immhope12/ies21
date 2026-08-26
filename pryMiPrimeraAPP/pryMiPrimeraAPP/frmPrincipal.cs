55face7fd11b47c6a9a0e1ddcc3bf2d2ee178398
Hope <im.the.hope01@gmail.com>
2026-08-25 23:35:38 -0300

    Add files via upload

---
+++ pryMiPrimeraAPP/pryMiPrimeraAPP/frmPrincipal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pryMiPrimeraAPP
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Papoi!!!");
+        }
+    }
+}
---
+++ pryPresentacionPerezIG/pryPresentacionPerezIG/frmCaso1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pryPresentacionPerezIG
+{
+    public partial class frmCaso1 : Form
+    {
+        public frmCaso1()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCaso1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPresentar_Click(object sender, EventArgs e)
+        {
+            
+
+            string datos =  $"Nombre: {txtNombre.Text}\n"+
+                            $"Apellido: {txtApellido.Text}\n"+
+                            $"Edad: {txtEdad.Text} \n"+
+                            $"Carrera {txtCarrera.Text}";
+
+
+
+            MessageBox.Show(datos, "Información Ingresada");
+
+        }
+
+        private void lblApellido_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
