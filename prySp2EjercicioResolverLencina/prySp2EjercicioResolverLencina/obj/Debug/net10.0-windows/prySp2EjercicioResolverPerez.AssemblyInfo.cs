f146835e5a3a3242dc3c256f7883eb99ae8b007a
immhope12 <im.the.hope01@gmail.com>
2026-09-16 00:46:21 -0300

    Fix

--- prySp2EjercicioResolverLencina/prySp2EjercicioResolverLencina/obj/Debug/net10.0-windows/prySp2EjercicioResolverPerez.AssemblyInfo.cs
+++ prySp2EjercicioResolverLencina/prySp2EjercicioResolverLencina/obj/Debug/net10.0-windows/prySp2EjercicioResolverPerez.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("prySp2EjercicioResolverLencina")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("prySp2EjercicioResolverPerez")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c826dfe03ab4d9d6a737a85b9d9746891f563a6")]
-[assembly: System.Reflection.AssemblyProductAttribute("prySp2EjercicioResolverLencina")]
-[assembly: System.Reflection.AssemblyTitleAttribute("prySp2EjercicioResolverLencina")]
+[assembly: System.Reflection.AssemblyProductAttribute("prySp2EjercicioResolverPerez")]
+[assembly: System.Reflection.AssemblyTitleAttribute("prySp2EjercicioResolverPerez")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
